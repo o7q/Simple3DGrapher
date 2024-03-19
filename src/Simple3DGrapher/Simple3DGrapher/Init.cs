9a3f427b2b06271a8e84ca2c6f438aa086461df1
o7q <jev.o7q@gmail.com>
2024-03-19 02:34:12 -0500

    Update Init.cs

--- src/Simple3DGrapher/Simple3DGrapher/Init.cs
+++ src/Simple3DGrapher/Simple3DGrapher/Init.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Simple3DGrapher
 {
     internal static class Init
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
